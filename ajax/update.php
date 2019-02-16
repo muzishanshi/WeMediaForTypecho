08d6203cbf854c470ac5ac703d381ac4c6fcd449
TypechoShop <typechoshop@qq.com>
2019-02-16 22:56:53 +0800

    Diamond

--- ajax/update.php
+++ ajax/update.php
@@ -1,6 +1,6 @@
 <?php
 //版本检测
 $version = isset($_POST['version']) ? addslashes($_POST['version']) : '';
-$version=file_get_contents('https://tongleer.com/api/interface/WeMedia.php?action=update&version='.$version);
+$version=file_get_contents('https://www.tongleer.com/api/interface/WeMedia.php?action=update&version='.$version);
 echo $version;
 ?>
